7f6ac8dd311536b3fb3a597880f0c680195b626b
Wattanajit <wattanajit.j@kkumail.com>
2021-02-20 17:21:36 +0700

    week5

--- week5/work5.py
+++ week5/work5.py
@@ -1,25 +1,5 @@
 #แบบฝึกหัด 5.1
-'''class nisit :
-    def __init__(self,name,surname,year,branch,sex) : 
-        self.name = name
-        self.surname = surname
-        self.year = year
-        self.branch = branch
-        self.sex = sex
-
-    def shownisit(self) :
-        print('Nisit properties')
-        print('name : ',self.name)
-        print('surname : ',self.surname)
-        print('year : ',self.year)
-        print('branch : ',self.branch)
-        print('sex : ',self.sex)
-
-x = nisit('Wattanajit','Jitsabai','1','Computer Education','mule')
-x.shownisit()'''
-
-#แบบฝึกหัด 5.1
-class nisit:
+'''class nisit:
     def __init__(self,name,province,year,department,sex):
         self.name = name
         self.province = province
@@ -45,6 +25,58 @@ class nisit:
         department = input('สาขา : ')
         sex = input('เพศ : ')
         return self(name,province,year,department,sex)
-
 x = nisit.info()
-x.show()
+x.show()'''
+
+#แบบฝึกหัด 5.2
+import os
+name_list = ['กะเพราหมู','กะเพราหมู+ไข่ข้น','กะเพราหมู+ไข่ข้น+ฮอทด็อก','กะเพราไก่','กะเพราไก่+ไข่ข้น','กะเพราไก่+ไข่ข้น+ฮอทด็อก']
+price_list = [39,55,60,39,55,60]
+class market :
+    def list_def(self):
+        for x in range(0,len(name_list)):
+            print(x+1,name_list[x],price_list[x],'บาท')
+    def choose(self):
+        print('**********กะเพราแล้วแต่**********')
+        print('\tแสดงรายการสินค้า[a]\n\tเพิ่มรายการสินค้า[s]\n\tออกจากระบบ[x]')
+    def input_choise(self):
+        global choise
+        choise = input('กรุณาเลือกคำสั่ง :\t')
+    def add_list(self):
+        while True:
+            print('เพิ่มรายการสินค้า หากต้องการ กรอก exit')
+            add_name = input('เพิ่มชื่อสินค้า :')
+            if add_name == 'exit':
+                break
+            else : 
+                add_price = input('เพิ่มราคาสินค้า :')
+                name_list.append(add_name)
+                price_list.append(add_price)
+            add = input ('ต้องการเพิ่มสินค้าอีกหรือไม่ [y/n] :')
+            if add == 'n' :
+                break
+            elif add == 'y' :
+                os.system('cls')
+
+while True:
+    x = market()
+    x.choose()
+    x.input_choise()
+    if choise == 'a' :
+        os.system('cls')
+        print('กรุณาเลือกคำสั่ง :\t',choise)
+        x.list_def()
+    if choise == 's' :
+        os.system('cls')
+        print('กรุณาเลือกคำสั่ง :\t',choise)
+        x.add_list()
+    if choise == 'x' :
+        os.system('cls')
+        print('กรุณาเลือกคำสั่ง :\t',choise)
+        close = input('ต้องการปิดโปรแกรมหรือไม่ [y/n] : ')
+        if close == 'n' :
+            os.system('cls')
+        if close == 'y' :
+            os.system('cls')
+            print('ปิดโปรแกรม')
+            break
